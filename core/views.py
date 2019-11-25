7b0280718ad7ae5fa2e269dcfb41057396165d69
Mohammed Anees <maniadevice89@gmail.com>
2019-11-25 23:14:12 +0000

    Water pump project

--- core/views.py
+++ core/views.py
@@ -30,6 +30,10 @@ def ramadan_2019():
 def bakrid_2019():
     return render_template('/bakrid-2019.html')
 
+@main_app.route('/water-pump-2019')
+def water_pump_2019():
+    return render_template('/water-pump-2019.html')
+
 @main_app.route('/accreditation')
 def accreditation():
     return render_template('accreditation.html')
